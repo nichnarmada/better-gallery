3153ef0b2ca094a0e96c7df714c420d6caaf029c
Nicholas Narmada <nichnarmada@gmail.com>
2025-06-28 15:49:43 -0500

    Add @tanstack/react-query and related dependencies; integrate QueryClientProvider in main.tsx for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from '@/components/theme-provider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
